52ad0fcb6ce3d54bc4ca88c0aec0b5e95ac95385
Steve99-coder <stevenderitu99@gmail.com>
2020-11-23 06:34:35 +0300

    create a method that appends the data for password

--- lock-test.py
+++ lock-test.py
@@ -2,7 +2,7 @@ class User:
     '''
     Class that holds website and password data for the users
     '''
-    class_list = []
+    lock_list = []
     def __init__(self,identity,data_id,website,weblogin,name):
         self.identitity = identity
         self.data_id = data_id
@@ -10,3 +10,8 @@ class User:
         self.web_key = weblogin
         self.user_name = name
     
+     def add_password(self):
+        '''
+        creating a method that stores the username for a user
+        '''
+        User.lock_list.append(self)
